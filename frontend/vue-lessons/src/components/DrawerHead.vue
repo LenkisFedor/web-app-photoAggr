cf78cd2b7d2d3d21cef31b28366af6483e5feea9
Федор <lenkis.fedor@gmail.com>
2024-06-22 21:36:18 +0300

    Меняем дизайн сайт

--- frontend/vue-lessons/src/components/DrawerHead.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="flex items-center gap-5">
-            <svg class="cursor-pointer opacity-30 rotate-180 transition  hover:opacity-100 hover:-translate-x-1" width="16" height="14" viewBox="0 0 16 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M1 7H14.7143" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M8.71436 1L14.7144 7L8.71436 13" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-            </svg>
-            <h2 class="text-2xl font-bold">Корзина</h2>
-    </div>
-</template>
